0e07d11d75f9d5fa09fbc982a2275c997a498172
eduvf <155389249+eduvf@users.noreply.github.com>
2024-09-08 21:18:54 +0200

    add parse

--- pera.js
+++ pera.js
@@ -1,6 +1,29 @@
+const arity = {
+    do: 1, print: 1, not: 1, inc: 1, dec: 1,
+    set: 2, while: 2, '=': 2, '<': 2,
+    on: 3, if: 3,
+};
+
 function lex(s) {
     const re = /[^()\s]+|\S/g;
     return ('do (' + s + ')').match(re);
+}
+
+function scan(tk) {
+    let e = [];
+    while (tk.length && tk[0] != ')')
+        e.push(parse(tk));
+    tk.shift();
+    return e;
+}
+
+function parse(tk) {
+    let t = tk.shift();
+    if (t == ')') return;
+    if (t == '(') return scan(tk);
+    if (t in arity)
+        return [t, ...Array(arity[t]).fill().map(() => parse(tk))];
+    return isNaN(t) ? t : +t;
 }
 
 let test = `
@@ -16,4 +39,4 @@ while < 0 i
   print dec i
 `;
 
-console.log(lex(test));
+console.log(parse(lex(test)));
